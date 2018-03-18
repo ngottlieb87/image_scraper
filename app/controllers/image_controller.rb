76c018e0f2ca62a516778a9ff6e05223aead603b
David Hale <david.hale7@gmail.com>
2018-03-18 10:19:20 -0700

    list links instead of actual images

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,7 +2,7 @@ class ImageController < ApplicationController
   def getImages
     require 'openssl'
     require 'open-uri'
-    
+
     doc = Nokogiri::HTML(open('http://www.snowtours.com', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
     @images = []
     images = doc.css('img')
