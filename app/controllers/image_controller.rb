06a324de961b6ab0f32f9de65e5057e50a4d2c88
David Hale <david.hale7@gmail.com>
2018-04-06 09:04:37 -0700

    target specific src keywords for images

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -3,12 +3,17 @@ class ImageController < ApplicationController
     require 'openssl'
     require 'open-uri'
 
-    doc = Nokogiri::HTML(open('http://www.snowtours.com', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+    doc = Nokogiri::HTML(open('https://www.zoetis.com/products-services/companion-animals/dogs.aspx', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
     @images = []
     images = doc.css('img')
 
     images.each do |image|
-      @images.push(image.values[0])
+      x = image.select { |k,v| ["name", "src"].include?(k) }
+      if (x[0][1].include? "dog")
+        @images.push(image.values[2])
+      else
+        @images.push('No Dog!!!')
+      end
     end
 
     render template: 'image/home'
